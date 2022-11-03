c6e661792c31d03a96ceb7bca0f127675806fcb9
Pushpendra Kumar <kumarpushp1992@gmail.com>
2022-11-03 13:14:04 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3000;
+const middleware = jsonServer.defaults();
+
+server.use(middleware);
+server.use(router);
+server.listen(port);
